7bd6bd832690109babc5c0c32a2a1ba62716f43e
hieplenet <hiep@hieple.net>
2016-01-14 11:15:52 +0700

    Add TripServiceShout

---
+++ Excercise 2 - TripService/C#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TripServiceKata.Tests
+{
+    [TestClass]
+    public class TripServiceShould
+    {
+        [TestMethod]
+        public void Throws_UserNotLoggedInException_If_User_Does_not_log_in_yet()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
