26ccdd780b0d357266f60e3339110c0a985c24d5
hieplenet <hiep@hieple.net>
2016-01-14 11:21:04 +0700

    Add default fail

--- Excercise 2 - TripService/C#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
+++ Excercise 2 - TripService/C#/TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
 {
@@ -8,6 +9,7 @@ namespace TripServiceKata.Tests
         [TestMethod]
         public void Throws_UserNotLoggedInException_If_User_Does_not_log_in_yet()
         {
+            new TripService().GetTripsByUser(null);
             Assert.IsTrue(true);
         }
     }
